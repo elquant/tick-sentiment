011db500ef24223e2d42ff658f5c37cd96a51cfe
Damian Fanaro <damianfanaro@gmail.com>
2020-05-04 22:22:32 +0200

    Rename twitter.csv file to tweets.csv

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 TRACK_KEYWORDS = ["trump"]
 TICKER_SYMBOLS = ["SPY", "QQQQ"]
 CONNECTION_STRING = "sqlite:///data/twitter.db"
-OUTPUT_FILENAME = "data/twitter.csv"
+OUTPUT_FILENAME = "data/tweets.csv"
 TABLE_NAME = "tweets"
 STOCKTWITS_API_URL = "https://api.stocktwits.com/api/2/streams/symbol"
